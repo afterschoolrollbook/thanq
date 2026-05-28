852efafc088c36d3bf70ff00e4fc251072c8033b
afterschoolrollbook <afterschool.rollbook@gmail.com>
2026-05-28 22:43:59 +0900

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import '@/styles/globals.css'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 프리뷰 URL 접속 시 운영 도메인으로 강제 이동
+const PROD_HOST = 'thanq-beta.vercel.app'
+const host = window.location.hostname
+if (host !== PROD_HOST && host !== 'localhost' && !host.startsWith('127.')) {
+  window.location.replace(
+    `https://${PROD_HOST}${window.location.pathname}${window.location.search}${window.location.hash}`
+  )
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
